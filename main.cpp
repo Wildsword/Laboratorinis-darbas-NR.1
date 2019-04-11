5edfebe7a8dcc75560f0fc65ef7d62fba6e407ab
Wildsword <47443089+Wildsword@users.noreply.github.com>
2019-04-11 22:54:05 +0300

    Add files via upload
    Tarpas didokas, bet dabar turiu laiko prisėsti normaliai :D

--- main.cpp
+++ main.cpp
@@ -1,137 +1,109 @@
 #include <iostream>
-#include <iomanip>
-#include <string>
 #include <cmath>
-#include <cctype>
-#include <stdio.h>
-#include<limits>
-#include<algorithm>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <fstream>
 
 using namespace std;
 
-
-int j, i, n, p, t=0, w=1, ats, c, g;
-    double h[15];
-    double med[15];
-    double b[15];
-    double o;
-    double nd[15];
-    string vardas[15], pavarde[15];
-
-
-void duomenys ( )
+struct student
 {
-        while (w==1)
-        {
-            j=0;
-            cout<<"Studento varda ir pavarde :"<<endl;
-            cin>>vardas[t]>>pavarde[t];
-            cout<<"Iveskite visus gautus pazymius uz namu darbus. Jei pazymiu nebera, veskite <0> :"<<endl;
-
-                    while(1)
-                    {
-                        cin>>nd[j];
-                        while(cin.fail() || nd[j]>10)
-                                {
-                                cin.clear();
-                                cin.ignore(256,'\n');
-                                cout<<"Veskite is naujo"<<endl;
-                                cin>>nd[j];
-                                }
-                        if(nd[j]==0)
-                            break;
-                        else
-                        {
-                            h[t]=h[t]+nd[j];
-                            j++;
-                        }
-                    }
-
-            if(j==0)
-                h[t]=0;
-            else
-            h[t]=h[t]/j;
-            cout<<"Iveskite pazymi uz egzamina :";
-            cin>>nd[j+1];
-            while(cin.fail() || nd[j+1]>10)
-                    {
-                    cin.clear();
-                     cin.ignore(256,'\n');
-                    cout<<"Veskite is naujo"<<endl;
-                    cin>>nd[j+1];
-                    }
-
-            b[t]=nd[j+1]*0.6+h[t]*0.4;
-
-            sort(nd, nd+j+1);
-
-
-        g=j+1;
-        if(g%2>0)
-        {
-            o=g;
-            o=o/2+0.5;
-            p=o;
-            med[t]=nd[p];
-        }
-            else
-            {
-
-                o=g/2;
-                p=o;
-                med[t]=(nd[p]+nd[p+1])/2;
-            }
-            t++;
-            cout<<" Ar norite testi? Veskite <1> jei taip. ";
-            cin>>w;
-        }
-
-
+    vector <int> nd;
+    string vardas, pavarde;
+    double mediana, balas;
 };
-
-
-void lentele1()
-{
-    cout <<setw(15)<<left<<"Vardas " <<setw(15)<<left<<"Pavarde "<<setw(20)<<right<<"Galutinis balas "<< endl;
-    cout.fill('-');
-    cout.width(50);
-    cout<<"-"<<endl;
-    cout.fill(' ');
-        for(i=0; i<t; i++)
-            cout<<setw(15)<<left<<vardas[i]<<setw(15)<<left<<pavarde[i]<<setw(20)<<setprecision(3)<<right<<b[i]<<endl;
-    cout.fill('-');
-    cout.width(50);
-    cout<<"-"<<endl;
-};
-
-void lentele2()
-{
-    cout <<setw(15)<<left<<"Vardas " <<setw(15)<<left<<"Pavarde "<<setw(15)<<right<<"Mediana "<< endl;
-    cout.fill('-');
-    cout.width(45);
-    cout<<"-"<<endl;
-    cout.fill(' ');
-        for(i=0; i<t; i++)
-            cout<<setw(15)<<left<<vardas[i]<<setw(15)<<left<<pavarde[i]<<setw(15)<<setprecision(3)<<right<<med[i]<<endl;
-    cout.fill('-');
-    cout.width(45);
-    cout<<"-"<<endl;
-}
-
 
 
 
 
 int main()
 {
+    student a;
+    vector<student>isvestis;
+    int pazimys=1, suma=0, E, i, ats;
+    string ArTesti={"y"};
+    while(ArTesti=="y")
+    {
+        cout << "Áveskite studento varda ir pavarde: " << endl;
+        cin>>a.vardas>>a.pavarde;
+        cout<<"Veskite pazymius uz namu darbus. Kuomet noresite baigti, spauskite <0>"<<endl;
+        pazimys=1;
+        a.nd.clear();
+        suma=0;
+            while(pazimys!=0)
+            {
+                cin>>pazimys;
+                a.nd.push_back(pazimys);
+                suma=suma+pazimys;
+            }
+    a.nd.erase(a.nd.end()-1);
+    cout<<"Koki pazymi gavo uz egzamina? "<<endl;
+    cin>>E;
+    if(cin.fail())
+    {
+        cout<<"Raide nera skaicius, veskite is naujo"<<endl;
+        cin>>E;
+    }
+    else
+        a.nd.push_back(E);
 
-    duomenys();
-    cout<<"Iveskite <1> jei norite suzinoti vidurki arba <2> jei Mediana"<<endl;
+    a.balas=E*1.0*0.6+suma/(a.nd.size()-1)*1.0*0.4;
+
+    sort(a.nd.begin(), a.nd.end());
+    for(i=0; i<a.nd.size(); i++)
+    cout<<a.nd[i]<<endl;
+
+    if(a.nd.size()%2==0)
+    {
+        a.mediana=(a.nd[a.nd.size()/2-1]+a.nd[a.nd.size()/2])/2;
+    }
+    else
+        a.mediana=a.nd[a.nd.size()/2];
+
+    cout<<a.vardas<<" "<<a.pavarde<<" "<<a.nd.size()<<" "<<suma<<" "<<a.balas<<" "<<a.mediana<<endl;
+
+    cout<<"Jei norite testi, spauskite <y>. Noredami pereiti prie duomenu isvedimo spauskite <n>"<<endl;
+    klausimas:
+    cin>>ArTesti;
+    if(ArTesti!="y" && ArTesti!="n")
+    {
+        cout<<"Tokio pasirinkimo nebuvo, veskite is naujo:"<<endl;
+        goto klausimas;
+    }
+        isvestis.push_back(a);
+    }
+
+    cout<<"Iveskite <1> jei norite suzinoti galutini ivertinima arba <2> jei Mediana"<<endl;
     cin>>ats;
     if(ats == 1)
-            lentele1();
+        {
+    cout <<setw(15)<<left<<"Vardas " <<setw(15)<<left<<"Pavarde "<<setw(20)<<right<<"Galutinis balas "<< endl;
+    cout.fill('-');
+    cout.width(50);
+    cout<<"-"<<endl;
+    cout.fill(' ');
+        //for(i=0; i<a.vardas.size(); i++)
+        for (student x : isvestis)
+            cout<<setw(15)<<left<<x.vardas<<setw(15)<<left<<x.pavarde<<setw(20)<<setprecision(3)<<right<<x.balas<<endl;
+    cout.fill('-');
+    cout.width(50);
+    cout<<"-"<<endl;
+        }
         else
-            lentele2();
+            {
+    cout <<setw(15)<<left<<"Vardas " <<setw(15)<<left<<"Pavarde "<<setw(15)<<right<<"Mediana "<< endl;
+    cout.fill('-');
+    cout.width(45);
+    cout<<"-"<<endl;
+    cout.fill(' ');
+        //for(i=0; i<a.vardas.size(); i++)
+        for (student x : isvestis)
+            cout<<setw(15)<<left<<x.vardas<<setw(15)<<left<<x.pavarde<<setw(15)<<setprecision(3)<<right<<x.mediana<<endl;
+    cout.fill('-');
+    cout.width(45);
+    cout<<"-"<<endl;
+}
 
     return 0;
 }
